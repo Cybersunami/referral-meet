de1498b88dd85090001cb9096f71be8360fcafc6
cybersunami <misawilliams@icloud.com>
2024-09-06 22:37:13 -0400

    Added pictures to landing page and enhanced profile page.

--- app/user/[id]/page.js
+++ app/user/[id]/page.js
@@ -3,15 +3,19 @@ import React from "react";
 import { useRouter } from "next/navigation";
 import UserProfile from "../../components/UserProfile";
 import Navbarloggedin from "@/app/components/navbarloggedin";
-import { useAuthRedirect } from "@/app/firebasefunctions/auth";
+import { useAuthRedirect } from "@/app/components/useauthredirect";
 
 const UserPage = ({params}) => {
-  useAuthRedirect()
   const router = useRouter();
   const { id } = params;
 
+  const { isAuthenticated, loading:authloading } = useAuthRedirect();
+  
   if (!id) return <div>Loading...</div>;
-
+  
+  if (!isAuthenticated) {
+    return null; // This is a safeguard; user will be redirected if not authenticated
+  }
   return (
   <>
 
